d2c46d647eb94782df9c44d90cdca0f3f1a7e941
mimi <mimiyumk@gmail.com>
2024-03-05 10:25:30 -0500

    Added ecommerce_web_vue frontend folder

---
+++ ecommerce_web_vue/src/views/HomeView.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="home">
+    <section class="hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">
+          Welcome to Nature Good
+        </p>
+        <p class="subtitle">
+          The best food store online
+        </p>
+      </div>
+    </section>
+
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">Latest Products</h2>
+      </div>
+
+      <ProductBox
+        v-for="product in latestProducts"
+        v-bind:key="product.id"
+        v-bind:product="product" />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import ProductBox from '@/components/ProductBox'
+
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      latestProducts: []
+    }
+  },
+  components: {
+    ProductBox
+  },
+  mounted() {
+    this.getLatestProducts()
+
+    document.title = ' Home | Nature Good'
+  },
+  methods: {
+    async getLatestProducts() {
+      this.$store.commit('setIsLoading', true)
+
+      await axios
+        .get('/api/v1/latest-products/')
+        .then(response => {
+          this.latestProducts = response.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+
+      this.$store.commit('setIsLoading', false)
+    }
+  }
+}
+</script>
